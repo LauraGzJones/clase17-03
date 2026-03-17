54b936232bb07dccd7a3601781e35bdd154109d2
LauraGzJones <lauragzjones@gmail.com>
2026-03-17 20:20:46 -0300

    hola

---
+++ index.py
@@ -0,0 +1,14 @@
+# APUNTES CLASE 17-03 
+# PARADIGMA ORIENTADO A OBJETOS  
+#VARIABLES 
+#input es la entradda 
+#salida  es print  la  variable va tener reglas que no puede ser en mayuscula, no  puede empezar por un numero, #
+# no puede contener espacios, 
+#no puede contener valor especiales ni tildes 
+# esto es en todos los lenguajes 
+#operador de asignacion = (py
+#operdador deasignacion alfanumerico seria ""
+nom = "" #declaración de la variable 
+# entrada del ux 
+nom = input("ingresa tu nombre: ")
+print ("Hola",nom, "bienvenidos al inframundo___,,,___(O.O)___,,,___")
