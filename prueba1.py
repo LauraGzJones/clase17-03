6314b719790b81cd1b2fd94f6129d9b7de4c1ed7
LauraGzJones <lauragzjones@gmail.com>
2026-03-17 20:37:42 -0300

    hola2

---
+++ prueba1.py
@@ -0,0 +1,11 @@
+#Desarrolle programa en Python q permita ingresar 5 datos de un ux y los despliegues de forma amable y intuitiva  
+
+#Nota: ocupe los 3 tipos de datos numérico, decimal y alfanumérico  
+
+#1 print con toda la informacion  
+
+#5 inputs 
+
+nombre = ""
+nombre = input("ingresa tu nombre: ")
+print ( "Bienvenid@",nombre, "al cuestionario ")
