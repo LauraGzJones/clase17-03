e321c46443c5b03fe69cd7a1ace65c8a275e6b2c
LauraGzJones <lauragzjones@gmail.com>
2026-03-18 15:19:32 -0300

    tarea

--- prueba1.py
+++ prueba1.py
@@ -8,4 +8,18 @@
 
 nombre = ""
 nombre = input("ingresa tu nombre: ")
-print ( "Bienvenid@",nombre, "al cuestionario ")
+print ( "Bienvenid@",nombre, "al cuestionario de postulación")
+edad = ""
+edad = input("ingresa tu edad: ")
+direccion = ""
+direccion = input("ingresa tu direccion: ")
+promedio = ""
+promedio = input("ingresa tu promedio de notas en enseñanza media: ")
+
+telefono = ""
+telefono = input("ingresa tu numero de telefono: ")
+print(nombre,"Ya quedaste corecctamente registrad@ tu edad es",edad,"años, tu direccion es",  direccion,"tu promedio de notas es",promedio,
+ "y tu numero de telefono es",telefono,"te estaremos contactando para informarte sobre el proceso de postulación, gracias por participar")
+
+
+
